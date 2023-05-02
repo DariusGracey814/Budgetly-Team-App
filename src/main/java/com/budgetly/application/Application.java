7e1d040fe2c8447fb182d2d0064129e2636e6c01
DariusGracey814 <Dariusgracey814@gmail.com>
2023-05-02 15:49:45 -0400

    updates

--- src/main/java/com/budgetly/application/Application.java
+++ src/main/java/com/budgetly/application/Application.java
@@ -8,13 +8,12 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-public class Application  {
+public class Application extends SpringBootServletInitializer {
 	
-//	@Override
-//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//		return application.sources(Application.class);
-	// extends SpringBootServletInitializer
-//	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
